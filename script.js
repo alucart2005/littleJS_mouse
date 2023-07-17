732ec5c58b884f70a61ab4b6d95a06d7cc55de10
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-07-16 21:50:15 -0500

    feat: End of the project

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const eyes = document.querySelectorAll('.eyes');
+const eyes = document.querySelector('.eyes');
 const head = document.querySelector('head');
-const ears = document.querySelectorAll('.ears');
+const ears = document.querySelector('.ears');
 const nose = document.querySelector('.nose');
 const snout = document.querySelector('.snout');
+const textXY = document.querySelector('.textXY');
+const text2 = document.querySelector('.text2');
 
 let cursorPos = { x: 0, y: 0 };
 let windowWidth = window.innerWidth;
@@ -15,17 +17,35 @@ function defineSizeScreen() {
 
 function mouseMove(e) {
   cursorPos = { x: e.clientX, y: e.clientY };
+  follow();
+}
+
+function touchMove(e) {
+  cursorPos = { x:e.targetTouche[0].elOffsetX, y:e.targetTouche[0].elOffsetY};
+  follow();
 }
 
 const followCursor = (el, xrelation, yrelation) => {
   const elOffset = el.getBoundingClientRect();
   const centerX = elOffset.x + elOffset.width / 2;
+  textXY.innerHTML = "valor X: " + Math.round(centerX);
   const centerY = elOffset.y + elOffset.height / 2;
+  textXY.innerHTML += " valor Y: " + Math.round(centerY);
   const distanceLeft = Math.round(((cursorPos.x - centerX)*100)/window.innerWidth);
+  text2.innerHTML = "valor X: " + distanceLeft;
   const distanceTop = Math.round(((cursorPos.y - centerY)*100)/window.innerHeight);
-  e.style.transform = `translate(${xrelation * distanceLeft}px, ${yrelation * distanceTop}px)`;
+  text2.innerHTML += " valor Y: " + distanceTop;
+  el.style.transform = `translate(${distanceLeft / xrelation}px, ${distanceTop /yrelation}px)`;
 };
 
 const follow = () => {
-  if (ears) 
+  if (ears) followCursor(ears, 0, 0);
+  if (head) followCursor(head, 6, 6);
+  if (eyes) followCursor(eyes, 1.8, 1.8);
+  if (snout) followCursor(snout, 1.5, 1.5);
+  if (nose) followCursor(nose, 1, 1);
 }
+
+window.addEventListener(`resize`, defineSizeScreen);
+window.addEventListener(`mousemove`, mouseMove);
+window.addEventListener(`touchmove`, touchMove);
